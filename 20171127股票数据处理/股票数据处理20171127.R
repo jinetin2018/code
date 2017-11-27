be66c50916e46dd29800126adfdb3b0991881c25
jinetin2018 <jinetin@qq.com>
2017-11-27 21:51:03 +0800

    增加股票代码

---
+++ 20171127股票数据处理/股票数据处理20171127.R
@@ -0,0 +1,29 @@
+library(quantmod)
+library(TTR)
+library(PerformanceAnalytics)
+library(ggplot2)
+library(scales)
+options(stringAsFactors = FALSE)
+
+symbols <- c("GSPC","HSI","000001.SS","601318.SS","600519.SS","000651.SZ","000333.SZ",
+             "000858.SZ","600887.SS","603993.SS","000002.SZ","600036.SS","600085.SS",
+             "603288.SS","000423.SZ","000538.SZ","600436.SS")
+suppressWarnings(getSymbols(symbols,src = "yahoo",from="2012-01-01"))
+
+#getSymbols(symbols,src="yahoo",from="2012-01-01")
+df <- merge(GSPC$GSPC.Adjusted,HSI$HSI.Adjusted,`000001.SS`$`000001.SS.Adjusted`,
+            `601318.SS`$`601318.SS.Adjusted`,`600519.SS`$`600519.SS.Adjusted`,
+            `000651.SZ`$`000651.SZ.Adjusted`,
+            `000333.SZ`$`000333.SZ.Adjusted`,`000858.SZ`$`000858.SZ.Adjusted`,
+            `600887.SS`$`600887.SS.Adjusted`,`603993.SS`$`603993.SS.Adjusted`,
+            `000002.SZ`$`000002.SZ.Adjusted`,`600036.SS`$`600036.SS.Adjusted`,
+            `600085.SS`$`600085.SS.Adjusted`,`603288.SS`$`603288.SS.Adjusted`,
+            `000423.SZ`$`000423.SZ.Adjusted`,`000538.SZ`$`000538.SZ.Adjusted`,
+            `600436.SS`$`600436.SS.Adjusted`)
+#names(df) <- c("纳斯达克指数","恒生指数","上证指数","中国平安","贵州茅台","格力电器","美的电器",
+#               "五粮液","伊利股份","洛阳钼业","万科A","招商银行","同仁堂","海天味业","东阿阿胶",
+#               "云南白药","片子癀")
+names(df) <- c("GSPC","HSI","000001.SS","601318.SS","600519.SS","000651.SZ","000333.SZ",
+               "000858.SZ","600887.SS","603993.SS","000002.SZ","600036.SS","600085.SS","603288.SS",
+               "000423.SZ","000538.SZ","600436.SS")
+head(df)
